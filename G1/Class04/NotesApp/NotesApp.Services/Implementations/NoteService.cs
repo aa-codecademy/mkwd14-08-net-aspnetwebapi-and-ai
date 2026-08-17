cdee2d5cebb25b7badc1ff6451f9bcdc478f2514
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2026-08-17 20:48:37 +0200

    Notes App update

---
+++ G1/Class04/NotesApp/NotesApp.Services/Implementations/NoteService.cs
@@ -0,0 +1,14 @@
+﻿using NotesApp.DataAccess.Interfaces;
+using NotesApp.Services.Interfaces;
+
+namespace NotesApp.Services.Implementations;
+
+public class NoteService : INoteService
+{
+    private readonly INoteRepository _noteRepository;
+
+    public NoteService(INoteRepository noteRepository)
+    {
+        _noteRepository = noteRepository;
+    }
+}
--- G1/Class04/NotesApp/NotesApp/Program.cs
+++ G1/Class04/NotesApp/NotesApp/Program.cs
@@ -1,3 +1,8 @@
+using NotesApp.DataAccess.Implementations;
+using NotesApp.DataAccess.Interfaces;
+using NotesApp.Services.Implementations;
+using NotesApp.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<INoteService, NoteService>();
+builder.Services.AddScoped<INoteRepository, NoteRepository>();
 
 var app = builder.Build();
 
