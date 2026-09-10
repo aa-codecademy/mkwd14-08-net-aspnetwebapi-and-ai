0220c1da185a056773fdf69bb42d43257bc0f963
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2026-09-10 12:19:17 +0200

    Generate random key

--- G1/Class12/NotesApp/NotesApp.Services/Configuration/JwtSettings.cs
+++ G1/Class12/NotesApp/NotesApp.Services/Configuration/JwtSettings.cs
@@ -3,6 +3,7 @@
 public class JwtSettings
 {
     // SecretKey => used to sign the JWT token, and it should be kept secret. It is used to create a digital signature for the token, which allows the server to verify that the token has not been tampered with. The secret key should be long, random, and stored securely (e.g., in environment variables or a secure vault).
+    // You can use this command in Git Bash to generate a random key: `openssl rand -base64 32`
     public string SecretKey { get; set; } = string.Empty;
     // Issuer => identifies the principal that issued the JWT. It is used to verify that the token was issued by a trusted source. The issuer is typically the URL or name of the application or service that generated the token.
     public string Issuer { get; set; } = string.Empty;
